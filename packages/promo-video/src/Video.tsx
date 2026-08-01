6d6e41b72cc737fba5afe83ca15e459ae74311a0
Stsgs1980 <stsgs1980@gmail.com>
2026-08-01 11:09:16 +0300

    feat(promo-video): add stats scene with dashboard metrics

--- packages/promo-video/src/Video.tsx
+++ packages/promo-video/src/Video.tsx
@@ -3,6 +3,7 @@ import { AbsoluteFill, Sequence } from "remotion";
 import { IntroScene } from "./scenes/IntroScene";
 import { HierarchyScene } from "./scenes/HierarchyScene";
 import { WorkflowScene } from "./scenes/WorkflowScene";
+import { StatsScene } from "./scenes/StatsScene";
 
 export const Video: React.FC = () => {
   return (
@@ -16,7 +17,9 @@ export const Video: React.FC = () => {
       <Sequence from={450} durationInFrames={300}>
         <WorkflowScene />
       </Sequence>
-      {/* StatsScene will be added in Task 7 */}
+      <Sequence from={750} durationInFrames={300}>
+        <StatsScene />
+      </Sequence>
       {/* OutroScene will be added in Task 8 */}
     </AbsoluteFill>
   );
