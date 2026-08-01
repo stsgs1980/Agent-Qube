aaaf3705017ec82617ac6447ea8f22ac47c55be3
Stsgs1980 <stsgs1980@gmail.com>
2026-08-01 10:54:01 +0300

    feat: scaffold Remotion promo-video package

---
+++ packages/promo-video/src/Video.tsx
@@ -0,0 +1,9 @@
+import { AbsoluteFill } from "remotion";
+
+export const Video: React.FC = () => {
+  return (
+    <AbsoluteFill style={{ backgroundColor: "#000000" }}>
+      {/* Scenes will be added in subsequent tasks */}
+    </AbsoluteFill>
+  );
+};
