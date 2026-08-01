0bb17a2949b70d2ae08213a7dfca12b0be07fb00
Stsgs1980 <stsgs1980@gmail.com>
2026-08-01 11:06:08 +0300

    feat(promo-video): add hierarchy scene with agent graph flythrough

--- packages/promo-video/src/Video.tsx
+++ packages/promo-video/src/Video.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AbsoluteFill, Sequence } from "remotion";
 import { IntroScene } from "./scenes/IntroScene";
+import { HierarchyScene } from "./scenes/HierarchyScene";
 
 export const Video: React.FC = () => {
   return (
@@ -8,7 +9,9 @@ export const Video: React.FC = () => {
       <Sequence from={0} durationInFrames={150}>
         <IntroScene />
       </Sequence>
-      {/* HierarchyScene will be added in Task 5 */}
+      <Sequence from={150} durationInFrames={300}>
+        <HierarchyScene />
+      </Sequence>
       {/* WorkflowScene will be added in Task 6 */}
       {/* StatsScene will be added in Task 7 */}
       {/* OutroScene will be added in Task 8 */}
